0cd86fada78bfbcd9894d1034a29aaf0ac51cf8f
JoalLin21 <1760070012@QQ.COM>
2025-12-20 00:27:56 +0800

    RA-10: Initialize ResumAI frontend with React + Vite (#5)

---
+++ frontend/src/utils/filePreview.js
@@ -0,0 +1,10 @@
+/**
+ * FilePreview Utility Functions - File Preview
+ * 
+ * TODO: Implement the following features:
+ * - generateFilePreview() - Generate file preview image
+ * - Support PDF, DOCX preview
+ * - Get file info (size, type, etc.)
+ */
+
+// TODO: Implement file preview functions
